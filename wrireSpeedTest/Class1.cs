368f46dfad1b60fa66702485f9f531b428da068c
Иван Салдайкин <vanyasls123@gmail.com>
2022-11-26 15:24:41 +0300

    Добавьте файлы проекта.

---
+++ wrireSpeedTest/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrireSpeedTest
+{
+    internal class write
+    {
+        public void wr(ConsoleKeyInfo key, string txt)
+        {
+            char[] a = txt.ToCharArray().ToArray();
+            string kkk = key.KeyChar.ToString();
+            
+            foreach (char i in a)
+            {
+                Console.WriteLine(a.ToString());
+                if (kkk == i.ToString())
+                {
+                    Console.WriteLine("dj");
+                }
+            }
+        }
+    }
+}
---
+++ wrireSpeedTest/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using wrireSpeedTest;
+
+write wr = new write();
+string txt = "привет Иван, как дела?";
+Console.WriteLine(txt);
+while (true)
+{
+    ConsoleKeyInfo key = Console.ReadKey(true);
+
+    wr.wr(key, txt);
+}
